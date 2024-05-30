f71d303709cac245ba73ed1477f41d7287ad8bbd
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2024-05-30 13:05:54 +0300

    Refactor: modify the condition to set the image profile automatically when a user gets registered.

--- WebAppCinephiles/src/main/java/com/example/WebAppCinephiles/Controller/HomeController.java
+++ WebAppCinephiles/src/main/java/com/example/WebAppCinephiles/Controller/HomeController.java
@@ -133,8 +133,10 @@ public class HomeController {
                 gotRegistered = false;
                 addWarningMessagesRegister(user);
                 return "redirect:/register/";
-            } else
+            } else {
+                user.setImageProfileSrc("https://i.pinimg.com/474x/76/4d/59/764d59d32f61f0f91dec8c442ab052c5.jpg");
                 userRepository.save(user);
+            }
         } else if (!isDataCorrect) {
             if (user.getUsername().isEmpty() || user.getPassword().isEmpty()) {
                 errorMessage = "You must write data!";
